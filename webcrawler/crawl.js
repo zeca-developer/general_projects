c74be281981bf92ee72deb6dc5c20e47c2373225
zeca-developer <zearlos@gmail.com>
2025-01-02 21:43:12 +0000

    Added getUrlsFromHtml Function and tests

--- webcrawler/crawl.js
+++ webcrawler/crawl.js
@@ -1,8 +1,52 @@
-const { url } = require("inspector");
+const {JSDOM}  = require('jsdom');
+/**
+ * 
+ */
+function getUrlFromHtml(htmlBody, baseUrl)
+{
+    const urls = [];
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a'); // this will return all <a> elements
+    for (const linkElement of linkElements)
+    {
+        
+        // relative URL 
+        if (linkElement.href.slice(0, 1) === '/') 
+        {
+            try 
+            {
+                const urlObj = new URL(`${baseUrl}${linkElement.href}`);
+                urls.push(urlObj.href);
+            } 
+            catch (error) 
+            {
+                console.log(`error with relative url: ${error.message}`)
+            }
+            
+        }
+        // absolute URL
+        else
+        {
+            try 
+            {
+                const urlObj = new URL(linkElement.href);
+                urls.push(linkElement.href);
+            } 
+            catch (error) 
+            {
+                console.log(`error with absolute url: ${error.message}`)
+            }
+            
+        } 
+        
+    }
+ 
+    return urls;  
+}
 
-
-/* Normalize different URL strings that point to the same page
-**/
+/*
+ * Normalize different URL strings that point to the same page
+ */
 function normalizeUrl(urlString)
 {
     const urlObj = new URL(urlString);
@@ -18,5 +62,6 @@ function normalizeUrl(urlString)
 // this will make available the functions to another files that want to use it
 module.exports = 
 {
-    normalizeUrl
+    normalizeUrl,
+    getUrlFromHtml
 } 
--- webcrawler/crawl.test.js
+++ webcrawler/crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeUrl} = require("./crawl.js");  // import function
+const {normalizeUrl, getUrlFromHtml} = require("./crawl.js");  // import function
 const {test, expect} = require("@jest/globals"); 
 
 test("normalizeUrl strip protocol", () => 
@@ -39,3 +39,78 @@ test("normalizeUrl strip http", () =>
 })
     
     
+test("getUrlFromHtml, absolute URL", () => 
+{
+    const inputHtmlBody = `
+    <html>
+        <body>
+            <a href="https://blog.boot.dev">
+                Boot.dev Blog
+            </a>
+        </body>
+    </html>
+    `;
+    const inputBaseUrl = "https://blog.boot.dev"
+    const actual_output = getUrlFromHtml(inputHtmlBody, inputBaseUrl);
+    const expected = ["https://blog.boot.dev/"];
+
+    expect(actual_output).toEqual(expected)
+})
+        
+    
+test("getUrlFromHtml, relative URL", () => 
+{
+    const inputHtmlBody = `
+    <html>
+        <body>
+            <a href="/relativepath/">
+                Boot.dev Blog
+            </a>
+        </body>
+    </html>
+    `;
+    const inputBaseUrl = "https://blog.boot.dev"
+    const actual_output = getUrlFromHtml(inputHtmlBody, inputBaseUrl);
+    const expected = ["https://blog.boot.dev/relativepath/"];
+
+    expect(actual_output).toEqual(expected)
+})
+    
+test("getUrlFromHtml, both relative and absolute URL", () => 
+{
+    const inputHtmlBody = `
+    <html>
+        <body>
+             <a href="https://blog.boot.dev">
+                Boot.dev Blog 1
+            </a>
+            <a href="/relativepath/">
+                Boot.dev Blog 2
+            </a>
+        </body>
+    </html>
+    `;
+    const inputBaseUrl = "https://blog.boot.dev"
+    const actual_output = getUrlFromHtml(inputHtmlBody, inputBaseUrl);
+    const expected = ["https://blog.boot.dev/", "https://blog.boot.dev/relativepath/"];
+
+    expect(actual_output).toEqual(expected)
+})
+
+test("getUrlFromHtml, invalid URL", () => 
+    {
+        const inputHtmlBody = `
+        <html>
+            <body>
+                 <a href="invalid>
+                    Boot.dev Blog invalid
+                </a>
+            </body>
+        </html>
+        `;
+        const inputBaseUrl = "https://blog.boot.dev"
+        const actual_output = getUrlFromHtml(inputHtmlBody, inputBaseUrl);
+        const expected = [];
+    
+        expect(actual_output).toEqual(expected)
+    })
