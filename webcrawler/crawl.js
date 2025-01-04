97bd8e65fd5c5284c533f2d3a100ae11ba543ad3
zeca-developer <zearlos@gmail.com>
2025-01-04 10:50:11 +0000

    Added main and crawlPage functions, crawl page recursively is working

--- webcrawler/crawl.js
+++ webcrawler/crawl.js
@@ -1,6 +1,67 @@
 const {JSDOM}  = require('jsdom');
+
+
+async function crawlPage(baseUrl, currentUrl, pages)
+{
+    console.log(`Crawling URL ${currentUrl}`);
+    const baseUrlObj = new URL(baseUrl);
+    const currentUrlObj = new URL(currentUrl);
+
+    if (baseUrlObj.hostname !== currentUrlObj.hostname) 
+    {
+        return pages;
+    }
+
+    const normalizedCurrentUrl = normalizeUrl(currentUrl);
+    
+
+    if (pages[normalizedCurrentUrl] > 0) 
+    {
+        pages[normalizedCurrentUrl]++;
+        return pages;
+    }
+    pages[normalizedCurrentUrl] = 1;
+    
+    try 
+    {
+        const response = await fetch(currentUrl); 
+        // Valid page
+        if (response.status > 399) 
+        {
+            console.log(`error in fetching url with status code: ${response.status }`)
+            return pages;
+        }
+
+        // Verify if we are receiving valid HTML
+        const contentType = response.headers.get("content-type");
+        if (!contentType.includes("text/html")) 
+        {
+            console.log(`No HTML response, content type = ${contentType}`);    
+        }
+        const htmlBody = await response.text();
+
+
+        const nextUrls = getUrlFromHtml(htmlBody, baseUrl);
+        for(const nextUrl of nextUrls)
+        {
+            pages = await crawlPage(baseUrl, nextUrl, pages);      
+        } 
+    } 
+    catch (error) 
+    {
+        console.log(`error in fetching url: ${error.message}`)
+    }
+
+    return pages;
+}
+
+
+
 /**
- * 
+ * This function retrieves all links (both relative and absolute) from an HTML
+ * @param {*} htmlBody - HTML 
+ * @param {*} baseUrl - Base URL 
+ * @returns array of URLs in the HTML
  */
 function getUrlFromHtml(htmlBody, baseUrl)
 {
@@ -9,7 +70,6 @@ function getUrlFromHtml(htmlBody, baseUrl)
     const linkElements = dom.window.document.querySelectorAll('a'); // this will return all <a> elements
     for (const linkElement of linkElements)
     {
-        
         // relative URL 
         if (linkElement.href.slice(0, 1) === '/') 
         {
@@ -63,5 +123,6 @@ function normalizeUrl(urlString)
 module.exports = 
 {
     normalizeUrl,
-    getUrlFromHtml
+    getUrlFromHtml,
+    crawlPage
 } 
--- webcrawler/main.js
+++ webcrawler/main.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+const {crawlPage} = require("./crawl.js");  // import function
+
+
+async function main()
+{
+    if (process.argv.length < 3) 
+    {
+        console.log("no website provided");
+        process.exit(1);
+    }
+    else if (process.argv.length > 3) 
+    {
+        console.log("too many command line inputs");
+        process.exit(1);
+    }
+    const baseUrl = process.argv[2];
+
+    console.log(`starting crawl ${baseUrl}`);
+    
+    const pages = await crawlPage(baseUrl, baseUrl, {});
+
+    for(const page of Object.entries(pages))
+        console.log(page)
+}
+
+
+main()
